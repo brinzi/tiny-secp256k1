36afb90c375ef00cb3155168ebdf94b0719837d3
pxr_64 <bbrinzoiu@gmail.com>
2023-11-30 14:25:19 +0000

    Update wasm path

--- src_ts/wasm_path.ts
+++ src_ts/wasm_path.ts
@@ -1,6 +1,9 @@
-import { URL, fileURLToPath } from "url";
+import * as nodePath from "path";
 
 export function path(wasmFilename: string): string {
-  const url = new URL(wasmFilename, import.meta.url);
-  return fileURLToPath(url);
+  // Since we know this file will only be used by cjs
+  // and we know that wasm file will always be in the parent dir
+  // We can translate to the parent directory without problem
+  const pathname = nodePath.join(__dirname, "..", wasmFilename);
+  return pathname;
 }
